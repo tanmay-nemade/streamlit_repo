203e3eac5366130faf3b92fafbd9f06ab63934d8
mandarha1 <124249535+mandarha1@users.noreply.github.com>
2023-03-03 20:32:04 +0530

    Update sfdemo1_secrets.py

--- sfdemo1_secrets.py
+++ sfdemo1_secrets.py
@@ -12,6 +12,7 @@ st.header("Snowflake Monitoring Framework")
 def create_session_object():
    
     connection_parameters = st.secrets["snowflake"]
+    st.write(
     session = Session.builder.configs(connection_parameters).create()
     return session
 
@@ -19,7 +20,7 @@ def create_session_object():
 # Create Snowpark DataFrames that loads data from Knoema: Environmental Data Atlas
 def load_data(session):
     # CO2 Emissions by Country
-    snow_df_co2 = session.table("WAREHOUSE_METERING_HISTORY_PROGRAM").select(col("WAREHOUSE_NAME"),to_decimal(col("CREDITS_USED"),10,0).as_("CREDITS_USED"))
+    snow_df_co2 = session.table("WAREHOUSE_METERING_HISTORY").select(col("WAREHOUSE_NAME"),to_decimal(col("CREDITS_USED"),10,0).as_("CREDITS_USED"))
     snow_df_co2 = snow_df_co2.filter(col("WAREHOUSE_NAME").isNotNull())
     snow_df_co2 = snow_df_co2.group_by(col("WAREHOUSE_NAME")).agg(sum('CREDITS_USED').as_("Total Credit Consumption"))
     snow_df_co2 = snow_df_co2.sort(col("Total Credit Consumption").desc())
