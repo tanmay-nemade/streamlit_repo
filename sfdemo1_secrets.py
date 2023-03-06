90d17105d62d79f2a2cdebadc2a542dfc634b61e
mandarha1 <124249535+mandarha1@users.noreply.github.com>
2023-03-06 13:24:15 +0530

    Update sfdemo1_secrets.py

--- sfdemo1_secrets.py
+++ sfdemo1_secrets.py
@@ -35,7 +35,7 @@ def load_data(session):
                        
             
         with col2:
-            st.subheader('')
+            st.write('Graphical Representation')
             st.bar_chart(snow_df_co2,x="WAREHOUSE_NAME")
             #edited_df = st.experimental_data_editor(snow_df_co2)                  
             
