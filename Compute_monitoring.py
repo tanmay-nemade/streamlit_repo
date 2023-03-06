42634f242d50cf68a1312abc71eb8b2ddc36bc90
mandarha1 <124249535+mandarha1@users.noreply.github.com>
2023-03-06 21:46:01 +0530

    Update Compute_monitoring.py

--- Compute_monitoring.py
+++ Compute_monitoring.py
@@ -46,9 +46,9 @@ def wh_data(session):
     with st.container():
         with col1:
             st.markdown("**:blue[Monthly Credit Consumtpion - Table]**")
-            st.dataframe(monthly_df)
+            st.dataframe(monthly_df,use_container_width=True)
             st.markdown("**:blue[Warehouse  Credit Consumtpion - Table]**")
-            st.dataframe(snow_df_co2)
+            st.dataframe(snow_df_co2,use_container_width=True)
             
                        
             
