dfd12bec967d9d49dee32fe8d94dec2839171bc4
mandarha1 <124249535+mandarha1@users.noreply.github.com>
2023-03-07 14:31:06 +0530

    Create quickstart1.py

---
+++ quickstart1.py
@@ -0,0 +1,53 @@
+# Snowpark
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.functions import avg, sum, col,lit
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+     page_title="Environment Data Atlas",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://developers.snowflake.com',
+         'About': "This is an *extremely* cool app powered by Snowpark for Python, Streamlit, and Snowflake Data Marketplace"
+     }
+)
+
+# Create Session object
+def create_session_object():
+    connection_parameters = {
+      "account": "nq94943.ap-south-1.aws",
+      "user": "mathavale",
+      "password": "Infosys123",
+      "role": "accountadmin",
+      "warehouse": "compute_wh"
+    }
+    session = Session.builder.configs(connection_parameters).create()
+    #print(session.sql('select current_warehouse(), current_database(), current_schema()').collect())
+    return session
+
+# Add header and a subheader
+st.header("Knoema: Environment Data Atlas")
+st.subheader("Powered by Snowpark for Python and Snowflake Data Marketplace | Made with Streamlit")
+  
+# Create Snowpark DataFrames that loads data from Knoema: Environmental Data Atlas
+def load_data(session):
+    # CO2 Emissions by Country
+    snow_df_co2 = session.table("SNOWFLAKE.ACCOUNT_USAGE_WAREHOUSE_METERING_HISTORY")
+    
+    # Convert Snowpark DataFrames to Pandas DataFrames for Streamlit
+    pd_df_co2  = snow_df_co2.to_pandas()
+    
+    # Use columns to display the three dataframes side-by-side along with their headers
+    col1, col2 = st.columns(2)
+    with st.container():
+        with col1:
+            st.subheader('CO2 Emissions by Country')
+            st.dataframe(pd_df_co2)
+    
+
+if __name__ == "__main__":
+    session = create_session_object()
+    load_data(session)
