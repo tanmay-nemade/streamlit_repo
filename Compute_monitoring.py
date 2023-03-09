5707ba5fd7ecb2461be8b529c0e639b5e29a9b32
mandarha1 <124249535+mandarha1@users.noreply.github.com>
2023-03-09 12:11:59 +0530

    Update Compute_monitoring.py

--- Compute_monitoring.py
+++ Compute_monitoring.py
@@ -79,7 +79,9 @@ def wh_data(session):
             
             if selection:
                 st.write("You selected:")
-                st.json(selection["selected_rows"][0],expanded = True)
+                data = json.load(selection["selected_rows"][0],expanded = True)
+                for value in data['Year-Month']:
+                    print(value, ":", data['Year-Month'][value])
                 
                 
                 
